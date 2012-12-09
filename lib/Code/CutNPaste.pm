874a21f8fdef01559bd013ddabea79017fe0a9d1
Ovid <curtis_ovid_poe@yahoo.com>
2012-12-09 11:07:30 +0100

    Shell of our CutNPaste detector module.

---
+++ lib/Code/CutNPaste.pm
@@ -0,0 +1,111 @@
+package Code::CutNPaste;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Code::CutNPaste - The great new Code::CutNPaste!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Code::CutNPaste;
+
+    my $foo = Code::CutNPaste->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Curtis "Ovid" Poe, C<< <ovid at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-code-cutnpaste at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Code-CutNPaste>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Code::CutNPaste
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Code-CutNPaste>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Code-CutNPaste>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Code-CutNPaste>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Code-CutNPaste/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Curtis "Ovid" Poe.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Code::CutNPaste
